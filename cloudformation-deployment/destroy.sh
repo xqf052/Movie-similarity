e627a7541e4cca8c70ab4a6999455f1ca7705199
Qifan <xuqifan052@gmail.com>
2022-03-07 08:02:42 +1100

    automation

--- cloudformation-deployment/destroy.sh
+++ cloudformation-deployment/destroy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "waiting 5 minute before destroy"
+sleep 300
+aws s3api delete-object --bucket movie-similarity-bucket-xqf052 --key movie.json
 aws cloudformation delete-stack --stack-name movie-similarity-stack
 aws ec2 delete-key-pair --key-name MyKey
